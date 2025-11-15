c31fd080fdc0aad371ec09382025370490aa092b
Samantha Placito Melendrez <smelendrez002@gmail.com>
2025-11-14 18:28:23 -0800

    finished function. will seperate later

--- slice/lib/chat_page.dart
+++ slice/lib/chat_page.dart
@@ -1,8 +1,10 @@
 import 'package:cloud_firestore/cloud_firestore.dart';
 import 'package:firebase_storage/firebase_storage.dart';
+import 'package:chat_bubbles/chat_bubbles.dart';
 import 'package:flutter/material.dart';
 import 'package:image_picker/image_picker.dart';
 import 'package:video_player/video_player.dart';
+import 'package:slice/widgets/video_bubble.dart';
 import 'dart:io';
 
 //this is not the official chats page. need to create one for sharing media
@@ -91,14 +93,84 @@ class _ChatPageState extends State<ChatPage>{
 
       //design
       return Scaffold(
+        backgroundColor: const Color.fromARGB(255, 233, 250, 221),
         appBar: AppBar(
           title: Text(widget.chatPartnerId),
           backgroundColor: Colors.green[700],
         ),
 
         body: Column(
+          children: [
+            Expanded(
+              child: StreamBuilder(
+                stream: messageRef.snapshots(), 
+                builder: (context, snapshot){
+                  if(!snapshot.hasData){
+                    return const Center(child: CircularProgressIndicator());
+                  }
+
+                  final docs = snapshot.data!.docs;
+
+                  return ListView.builder(
+                    controller: _scrollController,
+                    itemCount: docs.length,
+                    itemBuilder: (context, index){
+                      final msg = docs[index].data() as Map<String, dynamic>;
+                      final isMe = msg['senderId'] == widget.currUserId;
+
+                      //Imported text bubbles
+                      if(msg['mediaType'] == ""){
+                        return BubbleSpecialOne(
+                          isSender: isMe,
+                          text: msg['text'],
+                          color: isMe ? const Color(0xFFD0F6C1) : const Color(0xFFFFD8DF),
+                          textStyle: const TextStyle(fontSize: 16),
+                        );
+                      }
+
+                      //the image bubble
+                      if(msg['mediaType'] == "image"){
+                        return BubbleNormalImage(
+                          id: msg['senderId'],
+                          image: Image.network(
+                            msg['mediaUrl'],
+                            width: 200,
+                            height: 200,
+                            fit: BoxFit.cover,
+                          ),
+                          color: Colors.transparent,
+                        );
+                      }
+
+                      // TODO: video bubble
+                      if(msg['mediaType'] == "video"){
+
+                      }
+
+                      return const SizedBox.shrink();
+                    },
+                  );
+                },
+              ),
+            ),
+
+            //The text bar
+            MessageBar(
+              onSend: (text) => _sendMessage(text: text),
+              actions: [
+                IconButton(
+                  icon: const Icon(Icons.image, color: Colors.grey),
+                  onPressed: () => _pickMedia(true),
+                ),
+
+                IconButton(
+                  icon: const Icon(Icons.video_library, color: Colors.grey),
+                  onPressed: () => _pickMedia(false),
+                ),
+              ],
+            ),
+          ],
         ),
       );
   }
-
 }
