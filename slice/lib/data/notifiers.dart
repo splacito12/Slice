5ea99c00d18e71b9a327bdf57d70d4b819f073c2
Hooman Masroor <hoomanmasrour@gmail.com>
2025-11-18 16:44:13 -0800

    Revert "Login, Logout, and Basics of homepage are added."

--- slice/lib/data/notifiers.dart
+++
@@ -1,3 +0,0 @@
-import 'package:flutter/material.dart';
-
-ValueNotifier<int> currentPageNotifier = ValueNotifier(0);
--- slice/lib/login_page.dart
+++ slice/lib/login_page.dart
@@ -1,5 +1,4 @@
 import 'package:flutter/material.dart';
-import 'package:slice/services/auth/auth_service.dart';
 import 'signup_page.dart'; // so we can navigate to your existing sign-up screen
 
 class LoginPage extends StatefulWidget {
@@ -12,19 +11,6 @@ class LoginPage extends StatefulWidget {
 class _LoginPageState extends State<LoginPage> {
   final _emailController = TextEditingController();
   final _passwordController = TextEditingController();
-
-  void login() async {
-    // authentication service
-    final authService = AuthService();
-
-    try {
-      await authService.signInWithEmailPassword(_emailController.text, _passwordController.text);
-    }
-    catch (e) {
-      debugPrint(e.toString());
-    }
-
-  }
 
   @override
   Widget build(BuildContext context) {
@@ -97,7 +83,6 @@ class _LoginPageState extends State<LoginPage> {
                   child: ElevatedButton(
                     onPressed: () {
                       debugPrint('Login: ${_emailController.text}');
-                      login();
                     },
                     style: ElevatedButton.styleFrom(
                       backgroundColor: const Color(0xFF9BE69D),
