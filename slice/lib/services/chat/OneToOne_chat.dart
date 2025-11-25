d46397a44e2fb259757772aaec5bdff538e1480a
Samantha Placito Melendrez <smelendrez002@gmail.com>
2025-11-24 16:34:28 -0800

    changed up some functionality for the sake of testing

--- slice/lib/services/chat/OneToOne_chat.dart
+++ slice/lib/services/chat/OneToOne_chat.dart
@@ -2,7 +2,17 @@ import 'package:cloud_firestore/cloud_firestore.dart';
 import 'package:slice/services/keys_generate.dart';
 
 class OneToOneChat {
-  final FirebaseFirestore _firebaseFirestore = FirebaseFirestore.instance;
+  final FirebaseFirestore _firebaseFirestore;
+  final String Function() _keyGenerator;
+
+  OneToOneChat() : _firebaseFirestore = FirebaseFirestore.instance, _keyGenerator = generateMediaKey;
+
+  //created this in order for the test cases to pass
+  OneToOneChat.forTest(
+    this._firebaseFirestore,
+    this._keyGenerator,
+  );
+
 
   Future<String> directChat(String user1, String user2) async{
     final snapshot = await _firebaseFirestore
@@ -21,7 +31,7 @@ class OneToOneChat {
 
     //create chat
     final chatId = _firebaseFirestore.collection('chats').doc().id;
-    final mediaKey = generateMediaKey();
+    final mediaKey = _keyGenerator();
 
     await _firebaseFirestore.collection('chats').doc(chatId).set({
       'isGroup': false,
